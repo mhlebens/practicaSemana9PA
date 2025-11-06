a2ce5e591f798a952187e4ca810099edae457261
mhlebens <marifermatah@gmail.com>
2025-11-05 21:28:16 -0600

    Le cambié un mensaje de error a algo de registro model

--- PPA_Semana09/PPA_Semana09/Models/RegistroViewModel.cs
+++ PPA_Semana09/PPA_Semana09/Models/RegistroViewModel.cs
@@ -13,7 +13,7 @@ namespace PPA_Semana09.Models
         [Required(ErrorMessage = "El correo es obligatorio")]
         // Regla de la práctica: debe contener @ y .
         [RegularExpression(@".+@.+\..+",
-            ErrorMessage = "Debe contener '@' y '.'")]
+            ErrorMessage = "Debe ser un formato legible de correo electrónico")]
         public string Correo { get; set; }
 
         [Required(ErrorMessage = "El teléfono es obligatorio")]
